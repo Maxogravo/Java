d7b0686c44580652300c93549288f2f0d6629f64
Maxogravo <135208634+Maxogravo@users.noreply.github.com>
2023-06-01 10:12:24 +0100

    Update Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -1,3 +1,1 @@
-print ("You listen to the teacher drone on and on about some unimportant document that changed history.")
-name = input('Your name is:')
-print (name + "WAKE UP!")
+print ("This is a test")
